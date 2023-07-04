ad5696b9360a7ac9ab4181544555219fcb604a42
hattima-tim <irfanridwan20@gmail.com>
2023-07-04 21:42:36 +0600

    extract book data fetching logics

---
+++ app/bookApi.ts
@@ -0,0 +1,33 @@
+async function getBookData(bookId: string) {
+  const res = await fetch(
+    `https://3000-hattimatim-typeappserve-25257k7i7he.ws-us101.gitpod.io/books/${bookId}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  return res.json();
+}
+
+interface Book {
+  id: string;
+  auhtor: string;
+  bookName: string;
+  imgUrl: string;
+  pages: string[];
+}
+
+async function getAllBooksData(): Promise<Book[]> {
+  const res = await fetch(
+    `https://3000-hattimatim-typeappserve-25257k7i7he.ws-us101.gitpod.io/books`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  return res.json();
+}
+
+export { getAllBooksData, getBookData };
