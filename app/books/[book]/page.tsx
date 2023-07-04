312612841c519ee9b2057c19ce5fcb1e3a87fcb1
hattima-tim <irfanridwan20@gmail.com>
2023-07-04 21:44:31 +0600

    generate routes using bookId

--- app/books/[book]/page.tsx
+++ app/books/[book]/page.tsx
@@ -1,21 +1,13 @@
-export async function generateStaticParams() {
-  const books = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "11"];
+import { getAllBooksData, getBookData } from "@/app/bookApi";
 
-  return books.map((bookName) => {
+export async function generateStaticParams() {
+  const books = await getAllBooksData();
+
+  return books.map((book) => {
     return {
-      book: bookName,
+      book: book.id,
     };
   });
-}
-
-async function getBookData(book: string) {
-  const res = await fetch(`https://reqres.in/api/users?page=${book}`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data.");
-  }
-
-  return res.json();
 }
 
 export default async function Book({ params }: { params: { book: string } }) {
