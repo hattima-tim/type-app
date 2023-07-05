0fc375c476472c5ef0ad7e6be008dece1d6d21b8
hattima-tim <irfanridwan20@gmail.com>
2023-07-05 10:54:34 +0600

    get page data from backend

--- app/bookApi.ts
+++ app/bookApi.ts
@@ -30,4 +30,19 @@ async function getAllBooksData(): Promise<Book[]> {
   return res.json();
 }
 
-export { getAllBooksData, getBookData };
+async function getPageData(
+  bookId: string,
+  pageNumber: number
+): Promise<string> {
+  const res = await fetch(
+    `https://3000-hattimatim-typeappserve-25257k7i7he.ws-us101.gitpod.io/books/${bookId}/pages/${pageNumber}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  return res.json();
+}
+
+export { getAllBooksData, getBookData, getPageData };
